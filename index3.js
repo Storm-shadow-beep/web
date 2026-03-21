3cb5ef842000bf562126a0744532b31c58ed8dba
Storm-shadow-beep <ehudjf9@gmail.com>
2026-03-21 22:52:00 +0300

    about page start

--- index3.js
+++ index3.js
@@ -4,3 +4,8 @@ document.addEventListener('mousemove',(e)=>{
     dam.style.left =  e.clientX + 'px'
     dam.style.top =  e.clientY + 'px'
 })
+
+window.onload = () =>{
+    document.querySelector('h1').style.filter = 'blur(0px)';
+    document.getElementById('astro').style.filter = 'blur(0px)'
+}
