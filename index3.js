7a6cf1c1b1c8c02d10d7f3d658bb32200df22ab9
Storm-shadow-beep <ehudjf9@gmail.com>
2026-03-26 13:34:53 +0300

    work on responsiveness on nav and head

--- index3.js
+++ index3.js
@@ -1,4 +1,7 @@
 const dam = document.getElementById("cursor-light")
+const hamburger = document.getElementById('hamburger')
+const navmenu = document.getElementById('nav-menu')
+const navcon = document.getElementById("nav")
 
 document.addEventListener('mousemove',(e)=>{
     dam.style.left =  e.clientX + 'px'
@@ -9,3 +12,22 @@ window.onload = () =>{
     document.querySelector('h1').style.filter = 'blur(0px)';
     document.getElementById('astro').style.filter = 'blur(0px)'
 }
+
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active')
+    navmenu.classList.toggle('active')
+    navcon.style.height = "30%"
+    navcon.style.width = "80%"
+    navcon.style.borderRadius = "10%"
+
+})
+
+document.querySelectorAll('nav > ul > a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navmenu.classList.remove('active');
+        navcon.style.removeProperty('height')
+        navcon.style.removeProperty('border-radius')
+    });
+});
